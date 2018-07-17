03c80bcbf4f7276b9e1ea8110a0c4cc011b55d6f
Matthew Brett <matthew.brett@gmail.com>
2018-07-17 08:28:17 +0100

    Retire this repo

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # build-openblas
 
-A repository to build the OpenBLAS library, and upload to Rackspace at https://3f23b170c54c2533c070-1c8a9b3114517dc5fe17b7c3f8c63a43.ssl.cf2.rackcdn.com/
-
-Currently builds only for Windows.
+This repository retired in favor of
+https://github.com/matthew-brett/openblas-libs
--- prepare_local.bat
+++
@@ -1,8 +0,0 @@
-REM Run example build locally
-set OPENBLAS_COMMIT=5f998ef
-set OPENBLAS_ROOT=c:\opt
-set MSYS2_ROOT=C:\msys64
-set PYTHON_ARCH=32
-set OPENBLAS_SUFFIX=vanilla
-set VC9_ROOT=C:\Users\appveyor\AppData\Local\Programs\Common\Microsoft\Visual C++ for Python\9.0\VC
-git submodule update --init --recursive
